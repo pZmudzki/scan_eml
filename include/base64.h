4f721b96995bc69678197785a6e48f6335206975
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-04-11 15:32:53 +0200

    create base64 decoding function

---
+++ include/base64.h
@@ -0,0 +1,12 @@
+#ifndef SCAN_EML_BASE64_H
+#define SCAN_EML_BASE64_H
+
+/***********************************************
+decodes base64 format string into ASCII string
+@param plain encoded base64 format string
+@return ASCII string to be encoded
+***********************************************/
+char* base64_decode(char* cipher);
+
+
+#endif //SCAN_EML_BASE64_H
